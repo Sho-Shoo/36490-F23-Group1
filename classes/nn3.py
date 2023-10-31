10eb568e8da64ffb30eaf53c6ee0d7d007a30d33
Tianze Shou <shoutianze@outlook.com>
2023-10-31 17:12:56 -0400

    Implement NN3 with PyTorch

---
+++ classes/nn3.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+
+class NN3(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.linear_layers = nn.Sequential(
+            nn.Linear(in_features=21, out_features=32),
+            nn.ReLU(),
+            nn.Linear(in_features=32, out_features=16),
+            nn.ReLU(),
+            nn.Linear(in_features=16, out_features=8),
+            nn.ReLU(),
+            nn.Linear(in_features=8, out_features=1),
+        )
+
+    def forward(self, x):
+        return self.linear_layers(x)
