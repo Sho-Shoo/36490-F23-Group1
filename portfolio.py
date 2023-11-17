4ccf1ea51dad40e7036924e5daafe9a3a242a2dc
Tianze Shou <shoutianze@outlook.com>
2023-11-16 22:06:28 -0500

    Upload neural network portfolios

--- portfolio.py
+++ portfolio.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from classes.data_loader import DataLoader
+
+dataloader = DataLoader('data/usa_short.csv')
 
 
 def get_top_10_percent_indices(arr):
@@ -92,7 +95,7 @@ def getCoef(path, year):
     return coef
 
 
-obj_month_list = [int(f"{year}{month:02d}") for year in range(1995, 2022) for month in range(1, 13)]
+obj_month_list = [int(f"{year}{month:02d}") for year in range(1995, 2017) for month in range(1, 13)]
 
 def load_data_helper(month, path):
     data_train = data[data["date"].astype(str).str[:6] == str(month)]
@@ -108,6 +111,23 @@ def load_data_helper(month, path):
     pred_true_combined = np.vstack((preds,Y_train,size_train)).T
     pred_true_combined = pd.DataFrame(pred_true_combined)
     pred_true_combined.columns = ["pred","true","size"]
+    pred_true_combined['true_size'] = pred_true_combined[["true"]].multiply(pred_true_combined["size"], axis="index")
+    pred_true_combined = np.array(pred_true_combined)
+    return pred_true_combined
+
+
+def load_predictions(month, path):
+    data_train = dataloader.slice(month*100 + 1, (month+1)*100)
+    Y_train = dataloader.get_y(data_train)
+
+    with open(path, 'rb') as f:
+        all_preds = pickle.load(f)
+        preds = all_preds[obj_month_list.index(month)]
+
+    size_train = data_train["market_equity"].to_numpy()
+    pred_true_combined = np.vstack((preds, Y_train, size_train)).T
+    pred_true_combined = pd.DataFrame(pred_true_combined)
+    pred_true_combined.columns = ["pred", "true", "size"]
     pred_true_combined['true_size'] = pred_true_combined[["true"]].multiply(pred_true_combined["size"], axis="index")
     pred_true_combined = np.array(pred_true_combined)
     return pred_true_combined
@@ -130,8 +150,7 @@ def calc_portfolio(path):
         rets_ls_vw[i] = 0.5*(np.sum(pred_true_combined[highest_indices,3])/np.sum(pred_true_combined[highest_indices,2]) - 
                             np.sum(pred_true_combined[lowest_indices,3])/np.sum(pred_true_combined[lowest_indices,2]))
         rets_l_vw[i] = np.sum(pred_true_combined[highest_indices,3])/np.sum(pred_true_combined[highest_indices,2])
-        
-            
+
     # print("------------long-short----------")
     # print(rets_ls_vw)
     # print("------------long-----------")
@@ -140,18 +159,48 @@ def calc_portfolio(path):
     return rets_ls_vw, rets_l_vw
 
 
-eNetPath = 'outputs/elasticnet/models.pkl'
-eNet_long_short, eNet_long = calc_portfolio(eNetPath)
+def calc_portfolio_from_predictions(path):
+    rets_ls_vw = np.zeros(len(obj_month_list))
+    rets_l_vw = np.zeros(len(obj_month_list))
 
-with open('outputs/portfolio/long_short_eNet.pkl', 'wb') as f:
-    pickle.dump(eNet_long_short, f)
-with open('outputs/portfolio/long_eNet.pkl', 'wb') as f:
-    pickle.dump(eNet_long, f)
+    # for i in tqdm(range(len(obj_month_list))):
+    for i in tqdm(range(len(obj_month_list))):
+        pred_true_combined = load_predictions(obj_month_list[i], path)
+        num_stocks = pred_true_combined.shape[0] // 10
+        highest_indices = get_top_10_percent_indices(pred_true_combined[:, 0])
+        lowest_indices = get_bottom_10_percent_indices(pred_true_combined[:, 0])
 
-# nn3Path = 'outputs/nn3_50epochs/model_states.pkl'
-# nn3_long_short, nn3_long = calc_portfolio(nn3Path)
+        #value weighted portfolio return
+        #rets_ls_vw is the excess return of the value-weighted long-short portfolio
+        #rets_l_vw is the excess return of the value-weighted long portfolio
+        rets_ls_vw[i] = 0.5*(np.sum(pred_true_combined[highest_indices,3])/np.sum(pred_true_combined[highest_indices,2]) -
+                             np.sum(pred_true_combined[lowest_indices,3])/np.sum(pred_true_combined[lowest_indices,2]))
+        rets_l_vw[i] = np.sum(pred_true_combined[highest_indices,3])/np.sum(pred_true_combined[highest_indices,2])
 
-# with open('outputs/portfolio/long_short_nn3.pkl', 'wb') as f:
-#     pickle.dump(nn3_long_short, f)
-# with open('outputs/portfolio/long_nn3.pkl', 'wb') as f:
-#     pickle.dump(nn3_long, f)
+    return rets_ls_vw, rets_l_vw
+
+
+if __name__ == "__main__":
+    # eNetPredictions = 'outputs/elasticnet/predictions.pkl'
+    # eNet_long_short, eNet_long = calc_portfolio_from_predictions(eNetPredictions)
+    #
+    # with open('outputs/portfolio/long_short_eNet.pkl', 'wb') as f:
+    #     pickle.dump(eNet_long_short, f)
+    # with open('outputs/portfolio/long_eNet.pkl', 'wb') as f:
+    #     pickle.dump(eNet_long, f)
+
+    # nn3Path = 'outputs/nn3/predictions.pkl'
+    # nn3_long_short, nn3_long = calc_portfolio_from_predictions(nn3Path)
+    #
+    # with open('outputs/portfolio/long_short_nn3.pkl', 'wb') as f:
+    #     pickle.dump(nn3_long_short, f)
+    # with open('outputs/portfolio/long_nn3.pkl', 'wb') as f:
+    #     pickle.dump(nn3_long, f)
+
+    nn5Path = 'outputs/nn5_100epochs/predictions.pkl'
+    nn5_long_short, nn5_long = calc_portfolio_from_predictions(nn5Path)
+
+    with open('outputs/portfolio/long_short_nn5.pkl', 'wb') as f:
+        pickle.dump(nn5_long_short, f)
+    with open('outputs/portfolio/long_nn5.pkl', 'wb') as f:
+        pickle.dump(nn5_long, f)
